a9cb334b6be6e9d00b3d6a6d6ca24fb6fbe9c355
clash100a <clash100a@gmail.com>
2026-09-11 03:38:43 -0400

    packages update

---
+++ gsettings.sh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark'
+#gsettings set org.gnome.desktop.interface color-scheme 'default'
+#gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y'
+#gsettings set org.x.editor.preferences.editor scheme 'oblivion'
+
