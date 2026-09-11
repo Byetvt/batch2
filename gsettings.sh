28bf2de0f028edaa95d9c8803e2b65f1cb162c42
clash100a <clash100a@gmail.com>
2026-09-11 04:17:13 -0400

    packages update

--- gsettings.sh
+++ gsettings.sh
@@ -3,4 +3,12 @@ gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark'
 #gsettings set org.gnome.desktop.interface color-scheme 'default'
 #gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y'
 gsettings set org.x.editor.preferences.editor scheme 'oblivion'
+#
+# Acessibilty capslock, numlock show and beep
+#gsettings set org.cinnamon.desktop.a11y.keyboard togglekeys-enable-osd false
+gsettings set org.cinnamon.desktop.a11y.keyboard togglekeys-enable-osd true
+#gsettings set org.cinnamon.desktop.a11y.keyboard togglekeys-enable-beep false
+gsettings set org.cinnamon.desktop.a11y.keyboard togglekeys-enable-beep true
+ 
+
 
