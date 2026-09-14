204cfa597c6b919f1ca503e690e826a60eb7fe2f
clash100a <clash100a@gmail.com>
2026-09-14 01:20:50 -0400

    packages update

---
+++ ansible_refresh.sh
@@ -0,0 +1,6 @@
+wget -O master.zip https://github.com/Byetvt/batch2/archive/refs/heads/master.zip && ls -l master.zip
+unzip -o master.zip -d $HOME/ansible-config
+#The * wildcard in bash does not match hidden files (files starting with a dot, like .git or .gitignore) by default.
+mv $HOME/ansible-config/batch2-main/* $HOME/ansible-config/
+rm $HOME/ansible-config/batch2-main/.gitignore
+rmdir $HOME/ansible-config/batch2-main
